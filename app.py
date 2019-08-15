7f0d32eba3ca109cf0cbf562c069df9ca6340fc0
Ian <supersloth1066@gmail.com>
2019-08-15 15:49:40 +0100

    fixed problem in app where db was trying to be created

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -8,14 +10,10 @@ from resources.item import Item, Items
 from resources.store import Store, Stores
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' #doesn't haven't to be sqlite!
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'mysecretkey'
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity)
 app.config['PROPAGATE_EXCEPTIONS'] = True
@@ -29,4 +27,9 @@ api.add_resource(Stores, '/stores')
 if __name__ == '__main__':
     from db import db # we do it here to avoid circular imports
     db.init_app(app)
+
+    @app.before_first_request
+    def create_tables():
+        db.create_all()
+
     app.run(port='80', host='0.0.0.0', debug=False)
